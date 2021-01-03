196453cb68f5cd125442e6188a112b2bda7b0923
kaiweinan <kaiweinan@yahoo.com>
2021-01-03 16:13:02 -0500

    commit tree 6b2e786a45e2f136498e5c21687b14d172ae86cf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def split_word
+        self.word.split("")
+    end
+
+    def split_array(array)    
+        array.map{|e| e.split("")}
+    end
+
+    def match(array)
+        result = []
+        new_array = split_array(array)
+        new_array.each do |e| 
+            if e.sort == split_word.sort
+                result << e.join("")
+            end
+            result
+        end
+        result
+    end
+
+end 
